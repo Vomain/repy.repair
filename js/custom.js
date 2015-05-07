60febb61ce6832f7a4506872116ddb63ea617763
Thibault Kaspi <thibault.kaspi@gmail.com>
2015-05-07 01:18:22 +0100

    V1 !!

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,23 @@
 $(window).scroll(function () {
     var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
-    var windowHeight = $(window).height(); // get the height of the window
-    var docHeight = $(document).height();
     var separateTop = $("#portfolio").offset().top;
-    console.log(separateTop);
-    console.log(windowPos);
 
-    if (windowPos + 200 > separateTop && $("audio").attr("src") != "sound/repair_carriere.mp3") {
-        $("audio").attr("src", "sound/repair_carriere.mp3");
+    if (windowPos + 200 > separateTop && $("audio").attr("src") != "sound/repair_remix.wav") {
+        $("audio").attr("src", "sound/repair_remix.wav");
     }
 
     if (windowPos + 200 < separateTop && $("audio").attr("src") != "sound/repair.mp3") {
-        console.log("Coucou");
-        $("audio").attr("src", "sound/repair.mp3")
+        $("audio").attr("src", "sound/repair.mp3");
     }
+
+
 });
+
+/*$(".portfolio-link").click(function () {
+
+if ($(".portfolio-modal").attr("style") == "dislay: block;") {
+    $.each($('audio'), function () {
+        $(this).stop();
+    });
+}
+});*/
