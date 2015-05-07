7451705b1d6b32a856ee94b39c53040f9a05f864
Edouard Berthe <berthe.ed@gmail.com>
2015-05-07 10:50:14 +0100

    Adds random color to name in chat

--- app.js
+++ app.js
@@ -22,17 +22,18 @@ app.get('/', function (req,res) {
 
 io.sockets.on('connection', function (socket, pseudo) {
     // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
-    socket.on('nouveau_client', function(pseudo) {
-        console.log('Nouveau client !');
+    socket.on('nouveau_client', function(pseudo, color) {
+        console.log('Nouveau client : ' + pseudo);
         pseudo = ent.encode(pseudo);
         socket.pseudo = pseudo;
+        socket.color = color
         socket.broadcast.emit('nouveau_client', pseudo);
     });
 
     // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
     socket.on('message', function (message) {
         message = ent.encode(message);
-        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+        socket.broadcast.emit('message', {pseudo: socket.pseudo, color: socket.color, message: message});
     }); 
 });
 
